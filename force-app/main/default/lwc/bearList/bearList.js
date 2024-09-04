3438d3c819053659dbacd2f4a303ac48e1ca687f
Sarah Rubia <sarahrubia22@gmail.com>
2024-09-04 18:17:20 -0300

    feat: example of a wired apex call

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,26 +1,41 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 /** BearController.getAllBears() Apex method */
 import getAllBears from '@salesforce/apex/BearController.getAllBears';
 
 export default class BearList extends LightningElement {
-	bears;
-	error;
-	appResources = {
-		bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
-	};
-  
-	connectedCallback() {
-		this.loadBears();
-	}
 
-	loadBears() {
-		getAllBears()
-			.then(result => {
-				this.bears = result;
-			})
-			.catch(error => {
-				this.error = error;
-			});
-	}
+  
+  /**************************************
+   * 
+   ------------------- Imperative Apex using Connected Callback ----------------------
+  bears;
+  error;
+  appResources = {
+    bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
+  };
+  
+  connectedCallback() {
+    this.loadBears();
+  }
+ 
+  loadBears() {
+    getAllBears()
+      .then(result => {
+        this.bears = result;
+      })
+      .catch(error => {
+        this.error = error;
+      });
+  }
+  
+  ******************************************/
+
+  // Wired Apex
+
+  @wire(getAllBears) bears;
+
+  appResources = {
+    bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
+  }
 }
