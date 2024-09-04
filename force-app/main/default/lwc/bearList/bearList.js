d5e85141d33d1c2045445060ca9229745018ea11
Sarah Rubia <sarahrubia22@gmail.com>
2024-09-04 18:22:07 -0300

    feat: add search and change bearList to use wired apex

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,41 +1,29 @@
 import { LightningElement, wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
-/** BearController.getAllBears() Apex method */
-import getAllBears from '@salesforce/apex/BearController.getAllBears';
-
+/** BearController.searchBears(searchTerm) Apex method */
+import searchBears from '@salesforce/apex/BearController.searchBears';
 export default class BearList extends LightningElement {
 
-  
-  /**************************************
-   * 
-   ------------------- Imperative Apex using Connected Callback ----------------------
-  bears;
-  error;
-  appResources = {
-    bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
-  };
-  
-  connectedCallback() {
-    this.loadBears();
-  }
- 
-  loadBears() {
-    getAllBears()
-      .then(result => {
-        this.bears = result;
-      })
-      .catch(error => {
-        this.error = error;
-      });
-  }
-  
-  ******************************************/
+	searchTerm = '';
 
-  // Wired Apex
-
-  @wire(getAllBears) bears;
-
-  appResources = {
-    bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
-  }
+	@wire(searchBears, {searchTerm: '$searchTerm'})
+	bears;
+	appResources = {
+		bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
+	};
+  
+	handleSearchTermChange(event) {
+		// Debouncing this method: do not update the reactive property as
+		// long as this function is being called within a delay of 300 ms.
+		// This is to avoid a very large number of Apex method calls.
+		window.clearTimeout(this.delayTimeout);
+		const searchTerm = event.target.value;
+		// eslint-disable-next-line @lwc/lwc/no-async-operation
+		this.delayTimeout = setTimeout(() => {
+			this.searchTerm = searchTerm;
+		}, 300);
+	}
+	get hasResults() {
+		return (this.bears.data.length > 0);
+	}
 }
