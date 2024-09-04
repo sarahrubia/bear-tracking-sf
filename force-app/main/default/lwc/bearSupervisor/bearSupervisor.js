6a65cd15bac9e36bb12e2991655652ec8131f9b1
Sarah Rubia <sarahrubia22@gmail.com>
2024-09-04 18:05:22 -0300

    feat: create bearSupervisor LWC component

---
+++ force-app/main/default/lwc/bearSupervisor/bearSupervisor.js
@@ -0,0 +1,18 @@
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+// Import Bear object fields
+import SUPERVISOR_FIELD from '@salesforce/schema/Bear__c.Supervisor__c';
+
+const bearFields = [SUPERVISOR_FIELD];
+
+export default class BearSupervisor extends LightningElement {
+	@api recordId; // Bear Id
+
+	@wire(getRecord, { recordId: '$recordId', fields: bearFields })
+  bear;
+
+	get supervisorId() {
+    console.log('bear data: ' + JSON.stringify(this.bear.data))
+		return getFieldValue(this.bear.data, SUPERVISOR_FIELD);
+	}
+}
