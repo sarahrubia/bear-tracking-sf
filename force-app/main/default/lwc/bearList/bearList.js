6406bf48814f5f8a7e24f57b91cbbcc70ed3a8f7
Sarah Rubia <sarahrubia22@gmail.com>
2024-09-05 11:37:00 -0300

    feat: create bearMap and add communication with bearList

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,3 +1,5 @@
+import { publish, MessageContext } from 'lightning/messageService';
+import BEAR_LIST_UPDATE_MESSAGE from '@salesforce/messageChannel/BearListUpdate__c';
 import { NavigationMixin } from 'lightning/navigation';
 import { LightningElement, wire } from 'lwc';
 /** BearController.searchBears(searchTerm) Apex method */
@@ -5,10 +7,22 @@ import searchBears from '@salesforce/apex/BearController.searchBears';
 
 export default class BearList extends NavigationMixin(LightningElement) {
 	searchTerm = '';
+  bears;
 
-	@wire(searchBears, {searchTerm: '$searchTerm'})
-	bears;
+  @wire(MessageContext) messageContext;
 
+  @wire(searchBears, {searchTerm: '$searchTerm'})
+  
+  loadBears(result) {
+    this.bears = result;
+    if (result.data) {
+      const message = {
+        bears: result.data
+      };
+      publish(this.messageContext, BEAR_LIST_UPDATE_MESSAGE, message);
+    }
+  }
+  
 	handleSearchTermChange(event) {
 		// Debouncing this method: do not update the reactive property as
 		// long as this function is being called within a delay of 300 ms.
@@ -16,7 +30,7 @@ export default class BearList extends NavigationMixin(LightningElement) {
 		window.clearTimeout(this.delayTimeout);
 
 		const searchTerm = event.target.value;
-    
+
 		// eslint-disable-next-line @lwc/lwc/no-async-operation
 		this.delayTimeout = setTimeout(() => {
 			this.searchTerm = searchTerm;
